4a7dc4f4dab999143baa4247606089479805d61a
Shreyas Zare <shreyas@technitium.com>
2019-12-14 15:42:14 +0530

    assembly version updated for release

--- MeshCore/Properties/AssemblyInfo.cs
+++ MeshCore/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mesh Core")]
-[assembly: AssemblyDescription("A private, peer-to-peer, end-to-end encrypted instant messenger.")]
+[assembly: AssemblyDescription("A secure, anonymous, peer-to-peer instant messenger.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Technitium")]
 [assembly: AssemblyProduct("Mesh")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
